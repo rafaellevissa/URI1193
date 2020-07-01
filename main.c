e0e17be2dd9fd4fda9a84d5ef3085b64348d264d
Rafael Levi <rafaellevissa@hotmail.com>
2020-07-01 00:36:13 +0000

    to do convection

--- main.c
+++ main.c
@@ -2,45 +2,108 @@
 #include<stdlib.h>
 #include <string.h>
 
+char * bin2dec (char *pm, int i, char *pn);
+char * bin2hex (char *pm, int i, char *pn);
+char * dec2bin (char *pm, int i, char *pn);
+char * dec2hex (char *pm, int i, char *pn);
+char * hex2bin (char *pm, int i, char *pn);
+char * hex2dec (char *pm, int i, char *pn);
+
 int main(void) {
   int quantidade,i,j;
   char number[32];
+  char n[34];
   char type[3];
-  char *pointer;
+  char *pointer, *pointerBin, *pointerDec, *pointerHex, *pointerNumber;
   scanf("%d",&quantidade);
   
-  //col(0): binary, col(1): decimal, col(2): hexadecimal, col(3): origin number
-  char *matrizBin[quantidade][32];
-  char *matrizDec[quantidade][32];
-  char *matrizHex[quantidade][32];
-  char *matrizInput[quantidade][3];
+  char matrizBin[quantidade][32];
+  char matrizDec[quantidade][32];
+  char matrizHex[quantidade][32];
+  char matrizInput[quantidade][3];
 
   for(i=0;i<quantidade;i++){    
     scanf("%s",number);
     scanf("%s",type);
-    matrizInput[i][0]=type;
+    pointerNumber = number;
     if(strcmp(type,"bin")==0){         
-      matrizBin[i][0]=number;
-      //Bin2Dec(i, number);
-      //Bin2Hex(i, number); 
-    }               
-    if(strcmp(type,"dec")==0){  
-      matrizDec[i][0]=number;
-      //Dec2Bin(i, number);
-      //Dec2Hex(i, number); 
-    }       
-    if(strcmp(type,"hex")==0){
-      matrizHex[i][0]=number;
-      //Hex2Bin(i, number);
-      //Hex2Dec(i, number);
-    }    
+      pointer = &matrizBin[i][0];
+      strcpy(pointer,number);
+      pointer = &matrizInput[i][0];
+      strcpy(pointer,type); 
+      pointerDec = &matrizDec[i][0];
+      pointerHex = &matrizHex[i][0];
+      bin2dec(pointerDec,i,pointerNumber);
+      bin2hex(pointerHex,i,pointerNumber);   
+    }else{
+      if(strcmp(type,"dec")==0){  
+        pointer = &matrizDec[i][0];
+        strcpy(pointer,number);
+        pointer = &matrizInput[i][0];
+        strcpy(pointer,type);
+        pointerBin = &matrizBin[i][0];
+        pointerHex = &matrizHex[i][0];
+        dec2bin(pointerBin,i,pointerNumber);
+        dec2hex(pointerHex,i,pointerNumber);
+      }else{
+        if(strcmp(type,"hex")==0){
+          pointer = &matrizHex[i][0];
+          strcpy(pointer,number);
+          pointer = &matrizInput[i][0];
+          strcpy(pointer, type);
+          pointerBin = &matrizBin[i][0];
+          pointerDec = &matrizDec[i][0];
+          hex2bin(pointerBin,i,pointerNumber);
+          hex2dec(pointerDec,i,pointerNumber);
+        }
+      }
+    }
   }
-  for(i=0;i<quantidade;i++){
-    printf("\nBin %d:\n",i);
-    printf("%s \n",matrizBin[i][0]);
-    printf("Input %d:\n",i);
-    printf("%s \n",matrizInput[i][0]);    
-  }  
+
+  printf("Matriz:\n");
+
 
   return 0;
+}
+
+char * bin2dec ( char *pm, int i, char *pn){
+  printf("função bin2dec\n");
+  char c = 'g';
+  char *p = &c;
+  return p;
+}
+
+char * bin2hex (char *pm, int i, char *pn){
+  printf("função bin2hex\n");
+  char c = 'g';
+  char *p = &c;
+  return p;
+}
+
+char * dec2bin (char *pm, int i, char *pn){
+  printf("função dec2bin\n");
+  char c = 'g';
+  char *p = &c;
+  return p;
+}
+
+char * dec2hex (char *pm, int i, char *pn){
+  printf("função dec2hex\n");
+  char c = 'g';
+  char *p = &c;
+  return p;
+}
+
+char * hex2bin (char *pm, int i, char *pn){
+  printf("função hex2bin\n");
+  char c = 'g';
+  char *p = &c;
+  return p;
+}
+
+char * hex2dec (char *pm, int i, char *pn){
+  printf("função hex2dec\n");
+  char c = 'g';
+  char *p = &c;
+  return p;
 }
